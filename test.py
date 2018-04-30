95fc1322e47c73a567ec24de0c08664f8ec22c23
Yash Patel <Yash@Yashs-Air.home>
2018-04-30 08:50:22 -0400

    final

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from affine import Affine
 
 ciper = Affine()
 while True:
-<<<<<<< HEAD
     que = raw_input ('Did you want to ENCRYPTED than press E and DECRYPTED than press D. (e/d)')
     if que == 'e':
         text = raw_input('Text --> ')
@@ -16,20 +15,3 @@ while True:
         decrypted = ciper.decrypt(text1, keys1)
         print 'KEY A: {}\nKEY B: {}'.format(str(keys1[0]), str(keys1[1]))
         print 'DECRYPTED TEXT: '+ decrypted
-=======
-    que = raw_input('did you want to encryot or decrypt ? (e/d)')
-    if que == 'e':
-    text = raw_input('Text --> ')
-    keys = ciper.getRandomKeys()
-    crypted = ciper.crypt(text, keys)
-    print 'KEY A: {}\nKEY B: {}'.format(str(keys[0]), str(keys[1]))
-    print 'CRYPTED TEXT: '+ crypted
-    que = raw_input ('Did you decrypted? (y/n)')
-    elif que == 'd':    
-        text1 = raw_input('text--')
-        keys1 = cipher.getRandomKeys()
-        decrypted = ciper.decrypt(crypted, keys)
-        print decrypted
-    else:
-        return
->>>>>>> 8fa09d644b79f2aa108156f5f631b8039c4df750
